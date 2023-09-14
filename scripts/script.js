ee79956e00cd9b9c8a84737b3c7e89fe4ec23090
Danyil Zuiev <danyil.zuiev@cdw.com>
2023-09-14 18:41:20 +0300

    BDD run tests

--- scripts/script.js
+++ scripts/script.js
@@ -4,19 +4,5 @@ function adding(){
 
 function sayHi(hi){
     console.log(`say ${hi}`)
-<<<<<<< HEAD
     // need to add some fix
-=======
-}
-
-function work(){
-    console.log('work function')
-<<<<<<< HEAD
-    // work in progress
-<<<<<<< HEAD
->>>>>>> main
-=======
-=======
->>>>>>> 26cda5f (Create work)
->>>>>>> 92edba2 (message)
 }
